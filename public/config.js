f15723c47deaea11b091c395dab11c3463bbdcf1
yuyueshiwu <155555852+yuyueshiwu@users.noreply.github.com>
2024-01-05 22:47:15 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__PRE_CONFIG__ = {
-  header: 'Server Status',
+  header: 'Server Statu',
   subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
   footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
